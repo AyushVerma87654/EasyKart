b7861b837ac4cdfe3adf9bf269cf5f5a9b8af683
Ayush Verma <tgmcodeyogi@gmail.com>
2022-10-14 11:26:40 +0530

    New Api has been integrated and working

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -5,7 +5,7 @@ function ProductDetail({ products }) {
   return (
     <div className="sm:grid grid-cols-3 gap-2 space-y-2 md:space-y-0">
       {products.map(function (item) {
-        return <Product key={item.title} {...item} />;
+        return <Product {...item} key={item.id} />;
       })}
     </div>
   );
