1be9e726bcb2fb2da817f598c5c29e4a4687c2f1
Ayush Verma <tgmcodeyogi@gmail.com>
2022-10-17 13:16:57 +0530

    Small change in UI

--- Providers/UserProvider.jsx
+++ Providers/UserProvider.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { UserContext } from "../src/Context";
 import Loading from "../src/Loading";
 import axios from "axios";
--- src/ContextHoc.jsx
+++ src/ContextHoc.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { AlertContext, CartContext, UserContext } from "./Context";
 
 const ContextHoc =
